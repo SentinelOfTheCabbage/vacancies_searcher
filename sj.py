0ac6841903c447d7d2590b67a8e9f31a8c376f7a
Neil Suleymanov <nmsuleymanov@yandex.ru>
2021-05-06 12:10:44 +0300

    Now HTML processing in correct order

--- sj.py
+++ sj.py
@@ -19,32 +19,36 @@ def get_beautified_html(head):
     r'<strong \args\> -> select words with "<" and ">" brackets pairs'
 
     result = head
-    result = sub('&mdash;','~', result
-    result = sub('&ndash;','--', result)
-    result = sub('&bull;','>', result)
+    result = sub('&mdash;','➖', result)
+    result = sub('&ndash;','-', result)
+    result = sub('&bull;','🔸', result)
     result = sub('&quot;','"', result)
     result = sub('&nbsp;',' ', result)
     result = sub('&laquo;','"', result)
     result = sub('&raquo;','"', result)
     result = sub('&middot;','>', result)
-
-    result = sub('<span[ \&#a-zA-Z:;=\'\"\-0-9,.]*>','\n',result)
-    result = sub('</span>','',result)
+    result = sub('&hellip;','...', result)
 
     result = sub('<p[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','',result)
     result = sub('</p>','\n',result)
-    
+
+    result = sub('<br />','\n',result)
+
+    result = sub('[ ]{1,}',' ', result)
+    result = sub('<[/]{0,1}h[0-9]>','', result)
+
+    result = sub('(<span[ \&#a-zA-Z:;=\'\"\-0-9,.]*>){1,}','\n',result)
+    result = sub('(</span>){1,}','',result)
+
     result = sub('<ul[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','\n',result)
     result = sub('</ul>','\n',result)
-    
+
     result = sub('<li[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','-',result)
     result = sub('</li>','\n',result)
-    
-    result = sub('<br />','<span>',result)
-    
+
     result = sub('<strong[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','<',result)
     result = sub('</strong>','>',result)
-    
+
     result = sub('<em[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','',result)
     result = sub('</em>','',result)
 
@@ -56,13 +60,15 @@ def get_beautified_html(head):
 
     result = sub('<font[ \&#a-zA-Z:;=\'"\"\-0-9,.]*>','',result)
     result = sub('</font>','',result)
-    
-    result = result.replace('<span>', '\n')
-    result = sub('[\n]{1,}','\n',result)
-    result = sub('-[\n]{1,}','-',result)
-    result = sub('<[\n]{1,}','<',result)
-    return result
 
+#    result = result.replace('<span>', '\n')
+    result = sub('-[\n ]{1,}','-',result)
+#    result = sub('<[\n ]{1,}','<',result)
+    result = sub('[\n]{1,}','\n',result)
+    result = sub('[\n ]*<[ \n]*','\n\n<', result)
+    result = sub('[\n ]*>[ \n]*','>\n', result)
+    return result
+ #   return head
 
 def get_current_vacancies():
     current_vacancies = list()
@@ -75,7 +81,7 @@ def get_current_vacancies():
             query = get(url_pattern.format(str(i), str(100)))
             content = load_string(query.text)['content']
             current_vacancies += content
-    
+
     return current_vacancies
 
 def main():
@@ -85,17 +91,20 @@ def main():
     with open('id_list.json', 'r') as file:
         checked_vacancies = load_string(file.read())
 
-    current_vacancies = [job for job in current_vacancies if job['id'] not in checked_vacancies]
+    current_vacancies = [job for job in current_vacancies
+                         if job['id'] not in checked_vacancies]
+
     for pos, job in enumerate(current_vacancies):
         os.system('clear')
         print(f'{pos+1}/{len(current_vacancies)}')
         if 'header' in job['content']:
             print(
+                job['content']['header'],
                 f"\t! {job['content']['title']} !\n",
                 get_beautified_html(job['content']['header']),
-                f"\n==VACANCY ID = {job['id']}==", 
-                end='\n-----------------\n', 
-            )	    
+                f"\n==VACANCY ID = {job['id']}==",
+                end='\n-----------------\n',
+            )
             print(
                 'ENTER to skip',
                 'L to like it',
@@ -108,7 +117,7 @@ def main():
                 break
             if user_answer.lower() == 'l':
                 lovely_vacancies.append(job['id'])
-            checked_vacancies.append(job['id']) 
+            checked_vacancies.append(job['id'])
 
     with open('id_list.json', 'w') as file:
         dump(checked_vacancies, file)
